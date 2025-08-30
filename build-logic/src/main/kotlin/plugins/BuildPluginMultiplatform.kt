92232c8c40431e49f328b1b821510fa73bee8bae
Ilya Lisitsa <lex7555@yandex.ru>
2025-08-31 00:55:24 +0300

    Убрал лишнее

--- build-logic/src/main/kotlin/plugins/BuildPluginMultiplatform.kt
+++ build-logic/src/main/kotlin/plugins/BuildPluginMultiplatform.kt
@@ -5,7 +5,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.jvm.toolchain.JvmVendorSpec
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.repositories
 import org.gradle.kotlin.dsl.the
