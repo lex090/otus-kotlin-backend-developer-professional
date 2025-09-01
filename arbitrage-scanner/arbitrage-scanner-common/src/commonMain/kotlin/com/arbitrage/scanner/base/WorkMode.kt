c7492f1ef29a0f4ce4132b51095bafb802aca781
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-01 23:00:25 +0300

    Добавил базовые классы.

--- arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/base/WorkMode.kt
+++ arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/base/WorkMode.kt
@@ -1,7 +1,7 @@
 package com.arbitrage.scanner.base
 
-sealed interface WorkMode {
-    data object PROD : WorkMode
-    data object TEST : WorkMode
-    data object STUB : WorkMode
+enum class WorkMode {
+    PROD,
+    TEST,
+    STUB
 }
