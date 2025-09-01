c1693e15e240a9fa335606797479a20c1506dfdf
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-01 23:04:41 +0300

    Добавил базовые классы.

--- arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/models/ArbitrageOpportunityFilter.kt
+++ arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/models/ArbitrageOpportunityFilter.kt
@@ -1,10 +1,10 @@
 package com.arbitrage.scanner.models
 
 data class ArbitrageOpportunityFilter(
-    val dexCurrencyId: DexCurrencyId,
-    val dexExchangeId: DexExchangeId,
-    val dexChainId: DexChainId,
-    val cexCurrencyId: CexCurrencyId,
-    val cexExchangeId: CexExchangeId,
+    val dexCurrencyIds: Set<DexCurrencyId>,
+    val dexExchangeIds: Set<DexExchangeId>,
+    val dexChainIds: Set<DexChainId>,
+    val cexCurrencyIds: Set<CexCurrencyId>,
+    val cexExchangeIds: Set<CexExchangeId>,
     val spread: ArbitrageOpportunitySpread,
 )
