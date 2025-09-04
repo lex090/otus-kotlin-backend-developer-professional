fb4fcfa2074aaf6e1efdcf462ae86da67d54c412
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-05 00:31:54 +0300

    Подключил зависимости.

--- arbitrage-scanner/arbitrage-scanner-ktor/build.gradle.kts
+++ arbitrage-scanner/arbitrage-scanner-ktor/build.gradle.kts
@@ -1,4 +1,25 @@
 plugins {
     alias(libs.plugins.build.plugin.jvm)
+    alias(libs.plugins.ktor)
     alias(libs.plugins.kotlinx.serialization)
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+dependencies {
+    // Ktor
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.config.yaml)
+
+    // Koin
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger.slf4j)
+
+    // Logging
+    implementation(libs.logback.classic)
 }
