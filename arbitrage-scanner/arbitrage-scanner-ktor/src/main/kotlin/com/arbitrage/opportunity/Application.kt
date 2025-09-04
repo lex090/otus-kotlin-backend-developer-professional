ee51090b425a870d9cdca3b61b5bf42f0782b8a6
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-05 00:45:30 +0300

    Подключил зависимости.

---
+++ arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/Application.kt
@@ -0,0 +1,22 @@
+package com.arbitrage.opportunity
+
+import io.ktor.server.application.Application
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!2")
+        }
+    }
+}
