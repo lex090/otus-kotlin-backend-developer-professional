32ff3c9aec8b1d917305ea078315edc028265ed3
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-07 00:42:31 +0300

    Добавил роуты и проброс инстансов

--- arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/routing/RoutingConfiguration.kt
+++ arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/routing/RoutingConfiguration.kt
@@ -1,8 +1,19 @@
 package com.arbitrage.opportunity.routing
 
 import com.arbitrage.opportunity.routing.v1.routingV1
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.routing.routing
+import kotlinx.serialization.json.Json
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
-    routingV1()
+    val json: Json by inject<Json>()
+    routing {
+        install(ContentNegotiation) {
+            json(json = json)
+        }
+        routingV1()
+    }
 }
--- arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/routing/v1/RoutingV1.kt
+++ arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/routing/v1/RoutingV1.kt
@@ -1,28 +1,17 @@
 package com.arbitrage.opportunity.routing.v1
 
-import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.application.Application
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.routing.Routing
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
-import io.ktor.server.routing.routing
-import kotlinx.serialization.json.Json
-import org.koin.ktor.ext.inject
 
-fun Application.routingV1() {
-    val json: Json by inject<Json>()
-    routing {
-        install(ContentNegotiation) {
-            json(json = json)
-        }
-        route("/v1") {
-            route("arbitrage_opportunities") {
-                post("read") {
+fun Routing.routingV1() {
+    route("/v1") {
+        route("arbitrage_opportunities") {
+            post("read") {
+//                call.
+            }
+            post("update") {
 
-                }
-                post("update") {
-
-                }
             }
         }
     }
