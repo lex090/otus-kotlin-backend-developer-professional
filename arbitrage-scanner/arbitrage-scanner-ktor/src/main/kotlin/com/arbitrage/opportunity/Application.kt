c4dcacbab486b822bfc979ba9a10d87af61e6702
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-07 00:23:14 +0300

    Добавил модули

--- arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/Application.kt
+++ arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/Application.kt
@@ -1,22 +1,13 @@
 package com.arbitrage.opportunity
 
 import io.ktor.server.application.Application
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    configureKoin()
+    configureContextNegotiation()
     configureRouting()
-}
-
-fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!2")
-        }
-    }
 }
---
+++ arbitrage-scanner/arbitrage-scanner-ktor/src/main/kotlin/com/arbitrage/opportunity/KoinConfiguration.kt
@@ -0,0 +1,13 @@
+package com.arbitrage.opportunity
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import org.koin.core.logger.Level
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+fun Application.configureKoin() {
+    install(Koin) {
+        slf4jLogger(level = Level.INFO)
+    }
+}
