fd21016616d6ede3edec51fb84a2bd57bc0aeefb
Ilya Lisitsa <lex7555@yandex.ru>
2025-09-14 22:33:38 +0300

    Добавил базовые классы для логирования

---
+++ arbitrage-scanner/arbitrage-scanner-libs/arbitrage-scanner-lib-logging/src/commonMain/kotlin/com/arbitrage/scanner/libs/logging/ArbScanLoggerProvider.kt
@@ -0,0 +1,38 @@
+package ru.otus.otuskotlin.marketplace.logging.common.com.arbitrage.scanner.libs.logging
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+
+/**
+ * Инициализирует выбранный логер
+ *
+ * ```kotlin
+ * // Обычно логер вызывается вот так
+ * val logger = LoggerFactory.getLogger(this::class.java)
+ * // Мы создаем экземпляр логер-провайдера вот так
+ * val loggerProvider = MkpLoggerProvider { clazz -> mpLoggerLogback(clazz) }
+ *
+ * // В дальнейшем будем использовать этот экземпляр вот так:
+ * val logger = loggerProvider.logger(this::class)
+ * logger.info("My log")
+ * ```
+ */
+class ArbScanLoggerProvider(
+    private val provider: (String) -> ArbScanLogWrapper = { ArbScanLogWrapper.DEFAULT }
+) {
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(loggerId: String): ArbScanLogWrapper = provider(loggerId)
+
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(clazz: KClass<*>): ArbScanLogWrapper = provider(clazz.qualifiedName ?: clazz.simpleName ?: "(unknown)")
+
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(function: KFunction<*>): ArbScanLogWrapper = provider(function.name)
+}
+
