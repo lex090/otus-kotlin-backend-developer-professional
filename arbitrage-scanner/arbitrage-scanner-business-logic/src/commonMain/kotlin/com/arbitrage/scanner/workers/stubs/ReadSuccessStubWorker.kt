dc3186fa739ea20fdf5c744f0a45946ca535c69b
Ilya Lisitsa <lex7555@yandex.ru>
2025-10-24 21:28:51 +0300

    Добавлено логирование для успешных обработчиков команд read, recalculate и search в классе обработки стабов

--- arbitrage-scanner/arbitrage-scanner-business-logic/src/commonMain/kotlin/com/arbitrage/scanner/workers/stubs/ReadSuccessStubWorker.kt
+++ arbitrage-scanner/arbitrage-scanner-business-logic/src/commonMain/kotlin/com/arbitrage/scanner/workers/stubs/ReadSuccessStubWorker.kt
@@ -4,17 +4,25 @@ import com.arbitrage.scanner.ArbOpStubs
 import com.arbitrage.scanner.BusinessLogicProcessorImplDeps
 import com.arbitrage.scanner.base.State
 import com.arbitrage.scanner.context.Context
+import com.arbitrage.scanner.libs.logging.LogLevel
 import com.arbitrage.scanner.stubs.Stubs
 import com.crowdproj.kotlin.cor.ICorAddExecDsl
 import com.crowdproj.kotlin.cor.handlers.worker
+import kotlin.reflect.KFunction
+
+private val kFun: KFunction<Unit> =
+    ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>::readSuccessStubWorker
 
 fun ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>.readSuccessStubWorker(
     title: String,
 ) = worker {
     this.title = title
     on { this.stubCase == Stubs.SUCCESS && state == State.RUNNING }
+    val logger = config.loggerProvider.logger(kFun)
     handle {
-        arbitrageOpportunityReadResponse = ArbOpStubs.arbitrageOpportunity
-        state = State.FINISHING
+        logger.doWithLogging(id = requestId.toString(), level = LogLevel.DEBUG) {
+            arbitrageOpportunityReadResponse = ArbOpStubs.arbitrageOpportunity
+            state = State.FINISHING
+        }
     }
 }
