5947303a7b17407f9a1e2737ea110aec08e859a1
Ilya Lisitsa <lex7555@yandex.ru>
2025-11-02 00:12:50 +0300

    Добавил логику

---
+++ arbitrage-scanner/arbitrage-scanner-business-logic/src/commonMain/kotlin/com/arbitrage/scanner/workers/recalculate/PrepareRecalculateEventWorker.kt
@@ -0,0 +1,33 @@
+package com.arbitrage.scanner.workers.recalculate
+
+import com.arbitrage.scanner.BusinessLogicProcessorImplDeps
+import com.arbitrage.scanner.base.State
+import com.arbitrage.scanner.context.Context
+import com.arbitrage.scanner.libs.logging.LogLevel
+import com.arbitrage.scanner.models.RecalculateResult
+import com.crowdproj.kotlin.cor.ICorAddExecDsl
+import com.crowdproj.kotlin.cor.handlers.worker
+import kotlin.reflect.KFunction
+
+private val kFun: KFunction<Unit> =
+    ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>::prepareRecalculateResponseWorker
+
+fun ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>.prepareRecalculateResponseWorker(
+    title: String
+) = worker {
+    this.title = title
+    this.description = """
+        Этот обработчик ищет все возможные арбитражные возможности.
+    """.trimIndent()
+    on { state == State.RUNNING }
+    val logger = config.loggerProvider.logger(kFun)
+    handle {
+        logger.doWithLogging(id = requestId.toString(), level = LogLevel.INFO) {
+            recalculateResponse = RecalculateResult(
+                opportunitiesCount = arbOps.size,
+                processingTimeMs = executionTimeOfFindArbOps.inWholeMilliseconds
+            )
+            state = State.FINISHING
+        }
+    }
+}
