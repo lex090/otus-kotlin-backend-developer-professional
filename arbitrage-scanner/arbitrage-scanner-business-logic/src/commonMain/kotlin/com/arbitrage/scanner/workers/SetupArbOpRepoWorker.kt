98abb8e36c3811be86a2585c7c0f4a78197e3ad0
Ilya Lisitsa <lex7555@yandex.ru>
2025-11-02 00:12:58 +0300

    Добавил логику

---
+++ arbitrage-scanner/arbitrage-scanner-business-logic/src/commonMain/kotlin/com/arbitrage/scanner/workers/SetupArbOpRepoWorker.kt
@@ -0,0 +1,33 @@
+package com.arbitrage.scanner.workers
+
+import com.arbitrage.scanner.BusinessLogicProcessorImplDeps
+import com.arbitrage.scanner.base.State
+import com.arbitrage.scanner.base.WorkMode
+import com.arbitrage.scanner.context.Context
+import com.arbitrage.scanner.libs.logging.LogLevel
+import com.crowdproj.kotlin.cor.ICorAddExecDsl
+import com.crowdproj.kotlin.cor.handlers.worker
+import kotlin.reflect.KFunction
+
+private val kFun: KFunction<Unit> =
+    ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>::setupArbOpRepoWorker
+
+fun ICorAddExecDsl<Context, BusinessLogicProcessorImplDeps>.setupArbOpRepoWorker(
+    title: String
+) = worker {
+    this.title = title
+    this.description = """
+        Установка рабочего репозитория ArbOpRepo в зависимости от запрошенного режима работы
+    """.trimIndent()
+    on { state == State.RUNNING }
+    val logger = config.loggerProvider.logger(kFun)
+    handle {
+        logger.doWithLogging(id = requestId.toString(), level = LogLevel.INFO) {
+            arbOpRepo = when (workMode) {
+                WorkMode.PROD -> config.prodArbOpRepository
+                WorkMode.TEST -> config.testArbOpRepository
+                WorkMode.STUB -> config.stubArbOpRepository
+            }
+        }
+    }
+}
