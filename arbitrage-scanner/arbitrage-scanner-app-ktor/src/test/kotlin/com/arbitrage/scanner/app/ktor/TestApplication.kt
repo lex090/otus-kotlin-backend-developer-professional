ddc23b07227009603df449254276b6726cce21bb
Ilya Lisitsa <lex7555@yandex.ru>
2025-11-05 23:56:48 +0300

    Поправил логику стаб тестов.

--- arbitrage-scanner/arbitrage-scanner-app-ktor/src/test/kotlin/com/arbitrage/scanner/app/ktor/TestApplication.kt
+++ arbitrage-scanner/arbitrage-scanner-app-ktor/src/test/kotlin/com/arbitrage/scanner/app/ktor/TestApplication.kt
@@ -10,61 +10,95 @@ import com.arbitrage.scanner.api.v1.models.ArbitrageOpportunityRequestDebugStubs
 import com.arbitrage.scanner.api.v1.models.ArbitrageOpportunitySearchFilterApi
 import com.arbitrage.scanner.api.v1.models.ArbitrageOpportunitySearchRequest
 import com.arbitrage.scanner.api.v1.models.ArbitrageOpportunitySearchResponse
-import com.arbitrage.scanner.api.v1.models.CexToCexArbitrageOpportunityApi
+import com.arbitrage.scanner.api.v1.models.IRequest
 import com.arbitrage.scanner.api.v1.models.ResponseResult
-
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.testApplication
-import org.junit.jupiter.api.assertInstanceOf
+import org.junit.jupiter.api.assertNotNull
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TestApplication {
 
     @Test
-    fun `read request success stub test`() = testApplication {
-        application { module() }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-        val arbitrageOpportunityReadRequest = ArbitrageOpportunityReadRequest(
+    fun `read request success stub test2`() = testApplicationV2(
+        request = ArbitrageOpportunityReadRequest(
             debug = ArbitrageOpportunityDebugApi(
                 mode = ArbitrageOpportunityRequestDebugModeApi.STUB,
                 stub = ArbitrageOpportunityRequestDebugStubsApi.SUCCESS
             ),
             id = "123"
-        )
-        val response = client.post("v1/arbitrage_opportunities/read") {
-            contentType(ContentType.Application.Json)
-            setBody(arbitrageOpportunityReadRequest)
-        }
-
-        val arbitrageOpportunityReadResponse = response.body<ArbitrageOpportunityReadResponse>()
+        ),
+        method = "read",
+    ) { response ->
+        val responseBody = response.body<ArbitrageOpportunityReadResponse>()
         assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals(ResponseResult.SUCCESS, arbitrageOpportunityReadResponse.result)
-        assertInstanceOf<CexToCexArbitrageOpportunityApi>(arbitrageOpportunityReadResponse.arbitrageOpportunity)
-        when (val arbitrageOpportunity = arbitrageOpportunityReadResponse.arbitrageOpportunity) {
-            is CexToCexArbitrageOpportunityApi -> {
-                assertEquals(
-                    "123",
-                    arbitrageOpportunity.id
-                )
-            }
-        }
+        assertEquals(ResponseResult.SUCCESS, responseBody.result)
+
+        val arbitrageOpportunity = responseBody.arbitrageOpportunity
+        assertNotNull(arbitrageOpportunity)
+        assertEquals("123", arbitrageOpportunity.id)
     }
 
     @Test
-    fun `search request success stub test`() = testApplication {
+    fun `search request success stub test2`() =
+        testApplicationV2(
+            request = ArbitrageOpportunitySearchRequest(
+                debug = ArbitrageOpportunityDebugApi(
+                    mode = ArbitrageOpportunityRequestDebugModeApi.STUB,
+                    stub = ArbitrageOpportunityRequestDebugStubsApi.SUCCESS
+                ),
+                filter = ArbitrageOpportunitySearchFilterApi(
+                    cexTokenIds = emptySet(),
+                    cexExchangeIds = emptySet(),
+                    spread = 1.0,
+                ),
+            ),
+            method = "search",
+        ) { response ->
+            val responseBody = response.body<ArbitrageOpportunitySearchResponse>()
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(ResponseResult.SUCCESS, responseBody.result)
+
+            val arbitrageOpportunities = responseBody.arbitrageOpportunities
+            assertNotNull(arbitrageOpportunities)
+            assertEquals(1, arbitrageOpportunities.size)
+
+            val arbitrageOpportunity = arbitrageOpportunities.first()
+            assertEquals("123", arbitrageOpportunity.id)
+        }
+
+    @Test
+    fun `recalculate request success stub test`() =
+        testApplicationV2(
+            request = ArbitrageOpportunityRecalculateRequest(
+                debug = ArbitrageOpportunityDebugApi(
+                    mode = ArbitrageOpportunityRequestDebugModeApi.STUB,
+                    stub = ArbitrageOpportunityRequestDebugStubsApi.SUCCESS
+                ),
+            ),
+            method = "recalculate",
+        ) { response ->
+            val body = response.body<ArbitrageOpportunityRecalculateResponse>()
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(ResponseResult.SUCCESS, body.result)
+            assertEquals(1, body.opportunitiesCount)
+            assertEquals(100L, body.processingTimeMs)
+        }
+
+    private inline fun testApplicationV2(
+        request: IRequest,
+        method: String,
+        crossinline block: suspend (HttpResponse) -> Unit,
+    ): Unit = testApplication {
         application { module() }
 
         val client = createClient {
@@ -72,62 +106,12 @@ class TestApplication {
                 json()
             }
         }
-        val arbitrageOpportunitySearchRequest = ArbitrageOpportunitySearchRequest(
-            debug = ArbitrageOpportunityDebugApi(
-                mode = ArbitrageOpportunityRequestDebugModeApi.STUB,
-                stub = ArbitrageOpportunityRequestDebugStubsApi.SUCCESS
-            ),
-            filter = ArbitrageOpportunitySearchFilterApi(
-                cexTokenIds = emptySet(),
-                cexExchangeIds = emptySet(),
-                spread = 1.0,
-            ),
-        )
-        val response = client.post("v1/arbitrage_opportunities/search") {
+
+        val response = client.post("v1/arbitrage_opportunities/$method") {
             contentType(ContentType.Application.Json)
-            setBody(arbitrageOpportunitySearchRequest)
+            setBody(request)
         }
 
-        val arbitrageOpportunitySearchResponse = response.body<ArbitrageOpportunitySearchResponse>()
-        assertEquals(HttpStatusCode.Companion.OK, response.status)
-        assertEquals(ResponseResult.SUCCESS, arbitrageOpportunitySearchResponse.result)
-        assertEquals(1, arbitrageOpportunitySearchResponse.arbitrageOpportunities?.size)
-        arbitrageOpportunitySearchResponse.arbitrageOpportunities?.forEach { arbitrageOpportunity ->
-            when (arbitrageOpportunity) {
-                is CexToCexArbitrageOpportunityApi -> {
-                    assertEquals(
-                        "123",
-                        arbitrageOpportunity.id
-                    )
-                }
-            }
-        }
-    }
-
-    @Test
-    fun `recalculate request success stub test`() = testApplication {
-        application { module() }
-
-        val client = createClient {
-            install(ContentNegotiation) {
-                json()
-            }
-        }
-        val arbitrageOpportunityRecalculateRequest = ArbitrageOpportunityRecalculateRequest(
-            debug = ArbitrageOpportunityDebugApi(
-                mode = ArbitrageOpportunityRequestDebugModeApi.STUB,
-                stub = ArbitrageOpportunityRequestDebugStubsApi.SUCCESS
-            ),
-        )
-        val response = client.post("v1/arbitrage_opportunities/recalculate") {
-            contentType(ContentType.Application.Json)
-            setBody(arbitrageOpportunityRecalculateRequest)
-        }
-
-        val arbitrageOpportunityRecalculateResponse = response.body<ArbitrageOpportunityRecalculateResponse>()
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals(ResponseResult.SUCCESS, arbitrageOpportunityRecalculateResponse.result)
-        assertEquals(1, arbitrageOpportunityRecalculateResponse.opportunitiesCount)
-        assertEquals(100L, arbitrageOpportunityRecalculateResponse.processingTimeMs)
+        block.invoke(response)
     }
 }
