4982a2fe9b88fde6d0eb013e386076d24ee33289
Ilya Lisitsa <lex7555@yandex.ru>
2025-11-15 17:00:58 +0300

    Поправил код

--- arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/models/ArbitrageOpportunityId.kt
+++ arbitrage-scanner/arbitrage-scanner-common/src/commonMain/kotlin/com/arbitrage/scanner/models/ArbitrageOpportunityId.kt
@@ -3,13 +3,13 @@ package com.arbitrage.scanner.models
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class ArbitrageOpportunityId(val value: String = "") {
+value class ArbitrageOpportunityId(val value: String) {
 
     fun isDefault(): Boolean = this == NONE
 
     fun isNotDefault(): Boolean = !isDefault()
 
     companion object {
-        val NONE = ArbitrageOpportunityId()
+        val NONE = ArbitrageOpportunityId(value = "")
     }
 }
